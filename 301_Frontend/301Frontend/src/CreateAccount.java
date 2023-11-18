db4617478fa1cae40c8d8e9c9e31f072e53693be
Samantha Perry <samnperry@arizona.edu>
2023-11-18 16:55:04 -0700

    Finished Create Account

--- 301_Frontend/301Frontend/src/CreateAccount.java
+++ 301_Frontend/301Frontend/src/CreateAccount.java
@@ -1,13 +1,17 @@
+import java.io.IOException;
+
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CreateAccount {
 
     private String userType;
+    private App m = new App();
 
     @FXML
     private Button createAccountButton;
@@ -19,12 +23,29 @@ public class CreateAccount {
     private TextField createPassword;
 
     @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Label nonStudentError;
+
+    @FXML
     private ChoiceBox<String> userSelect;
 
     @FXML
-    void accountCreate(ActionEvent event) {
-
+    void accountCreate(ActionEvent event) throws IOException {
+       if (createEmail.getText().isEmpty() || createPassword.getText().isEmpty() || userSelect.getValue() == null){
+        errorLabel.setText("Please enter all the fields");
+        nonStudentError.setText("");
+       }
+       else if(userSelect.getValue().equals("Non-Student")){
+        errorLabel.setText("");
+        nonStudentError.setText("If you are a Non-Student, please contact University Staff to create an account for you.");
+       }
+       else if (userSelect.getValue().equals("Student") && !(createEmail.getText().isEmpty()) && !(createPassword.getText().isEmpty())){
+        m.changeScene("LoginScene.fxml");
+       }
     }
+
     private void handleChoiceBoxSelection() {
         userType = userSelect.getValue();
         return;
@@ -32,9 +53,8 @@ public class CreateAccount {
 
     public void initialize() {
         userSelect.setItems(FXCollections.observableArrayList("Student", "Non-Student"));
-        userSelect.setValue("Student");
 
-        // create listener for box selection
+        //Create listener for box selection
         userSelect.setOnAction(event -> handleChoiceBoxSelection());
     }
 
