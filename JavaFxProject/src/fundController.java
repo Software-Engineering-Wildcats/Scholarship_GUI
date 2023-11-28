b4a9d07d3c773e24a36941261dfc8d1d47895a71
phoenixrgarcia <phoenixrgarcia@gmail.com>
2023-11-27 23:16:33 -0700

    Update fundController.java

--- JavaFxProject/src/fundController.java
+++ JavaFxProject/src/fundController.java
@@ -2,14 +2,9 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 public class fundController {
     App m = new App();
