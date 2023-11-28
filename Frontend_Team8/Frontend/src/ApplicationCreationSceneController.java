6921eda048c1f419947b916a82d72a9c4bc381e2
phoenixrgarcia <phoenixrgarcia@gmail.com>
2023-11-28 00:45:26 -0700

    New Project

---
+++ Frontend_Team8/Frontend/src/ApplicationCreationSceneController.java
@@ -0,0 +1,121 @@
+import java.io.File;
+import java.io.IOException;
+import Backend.StudentApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class ApplicationCreationSceneController {
+
+    StudentApplication currApplication = new StudentApplication();
+
+    String scholarshipName;
+
+    App m = new App();
+
+    File selectedFile;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextArea ApplicantEssay;
+
+    @FXML
+    private TextArea ApplicantAcademicAche;
+
+    @FXML
+    private CheckBox ApplicantFinancialAid;
+
+    @FXML
+    private TextField ApplicantMajor;
+
+    @FXML
+    private TextField ApplicantMinor;
+
+    @FXML
+    private TextField ApplicantName;
+
+    @FXML
+    private TextField ApplicantNetID;
+
+    @FXML
+    private TextField ApplicantStudentID;
+
+    @FXML
+    private Button ApplicationSubmit;
+
+    @FXML
+    private Button docUpload;
+
+    @FXML
+    private Label FilePath;
+
+    @FXML
+    private Label errorMsg;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    void backScene(ActionEvent event) throws IOException {
+        m.changeScene("ApplicantGUI.fxml");
+    }
+
+    @FXML
+    void btnApplicationSubmition(ActionEvent event) throws IOException {
+        if (!ApplicantName.getText().isEmpty() && !ApplicantNetID.getText().isEmpty()
+                && !ApplicantStudentID.getText().isEmpty() && !ApplicantMajor.getText().isEmpty()
+                && !ApplicantMinor.getText().isEmpty()
+                && !ApplicantAcademicAche.getText().isEmpty()
+                && (ApplicantFinancialAid.isSelected() || !ApplicantFinancialAid.isSelected())
+                && !ApplicantEssay.getText().isEmpty()) {
+            currApplication = new StudentApplication(ApplicantName.getText(), ApplicantNetID.getText(),
+                    ApplicantStudentID.getText(), ApplicantMajor.getText(), ApplicantMinor.getText(),
+                    ApplicantAcademicAche.getText(), ApplicantFinancialAid.isSelected(), ApplicantEssay.getText());
+
+            // Would be sent to the backend here using the scholarship name as the key
+            System.out.println(currApplication.toString());
+            System.out.println("ScholarshipName = " + scholarshipName);
+            if (selectedFile.exists()) {
+                System.out.println(selectedFile.getName());
+            }
+
+            m.changeScene("ApplicantGUI.fxml");
+        } else {
+            errorMsg.setText("Please enter all the fields in the application.");
+        }
+
+    }
+
+    @FXML
+    void btnDocUpload(ActionEvent event) {
+        Stage m = new Stage();
+
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("PDF Files (*.pdf)", "*.pdf");
+        FileChooser file = new FileChooser();
+        file.getExtensionFilters().add(extFilter);
+        file.setTitle("Open File");
+        selectedFile = file.showOpenDialog(m);
+
+        if (file != null) {
+            Label pthLabel = (Label) FilePath;
+            pthLabel.setText(selectedFile.getName());
+
+        }
+
+    }
+
+    void setScholarshipName(String name) {
+        scholarshipName = name;
+    }
+
+}
---
+++ Frontend_Team8/Frontend/src/Backend/User.java
@@ -0,0 +1,68 @@
+package Backend;
+
+public class User {
+    private String email;
+    private String password;
+    private String name;
+    private int typeUser;
+
+    public User(String email, String password, String name, int typeUser) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.typeUser = typeUser;
+    }
+
+    public User(String email, String password, String name, String typeUser) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        setUserType(typeUser);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUserType() {
+        return typeUser;
+    }
+
+    public void setUserType(String userType) {
+        if (userType == "Applicant") {
+            typeUser = 1;
+        } else if (userType == "Admin") {
+            typeUser = 2;
+        } else if (userType == "Reviewer") {
+            typeUser = 3;
+        } else if (userType == "Sponsor") {
+            typeUser = 4;
+        } else if (userType == "Stewer") {
+            typeUser = 5;
+        } else if (userType == "Support") {
+            typeUser = 6;
+        } else {
+            typeUser = 1;
+        }
+    }
+}
